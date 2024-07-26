c2cdc3a5084294a874837eec5d4942b3cce8e5be
Eugenio Calandrini <calandrini.1278192@studenti.uniroma1.it>
2024-07-26 09:51:00 +0200

    created folder with database configuration file

---
+++ database_configuration/log.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `log` (
+`test_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Test ID',
+`seq_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'DataPoint',
+`computer_name` varchar(255) NOT NULL DEFAULT '' COMMENT '',
+`chl_id` varchar(255) NOT NULL DEFAULT '' COMMENT 'CH ID',
+`log_time` varchar(255) DEFAULT NULL COMMENT 'Time',
+`barcode` varchar(255) DEFAULT NULL COMMENT 'Cell bar code',
+`log_code` varchar(255) DEFAULT NULL COMMENT 'Log code',
+`log_level` varchar(255) DEFAULT NULL COMMENT 'Class',
+`log_event` varchar(255) DEFAULT NULL COMMENT 'Event',
+`upload_time` varchar(255) DEFAULT NULL COMMENT 'Upload time',
+PRIMARY KEY ( `test_id`,`seq_id`,`computer_name`,`chl_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
